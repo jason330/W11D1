929d8b55642089ee8b7ccdcd519c95f6fc99bd8b
shihyulung <shihyulung@yahoo.com.tw>
2022-10-17 13:31:44 -0700

    add inputs

---
+++ react-form/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+    <h1>Hello from App</h1>
+    <form>
+    <input type="text" placeholder="Username"></input>
+    <input type="text" placeholder="Username"></input>
+    <input type="text" placeholder="Username"></input>
+    <select>
+      <option value="home">Home</option>
+      <option value="work">Work</option>
+      <option value="mobile">Mobile</option>
+    </select>
+
+    <button></button>
+    </form>
+    </>
+  );
+}
+
+export default App;
